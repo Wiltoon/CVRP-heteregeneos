37e8b457a0f431308c995de399c469cfe9b87af1
Wilton <dev3@evop.com.br>
2022-03-08 22:43:49 -0400

    Realizar testes pendentes

--- CVRP-heterogeneos.cpp
+++ CVRP-heterogeneos.cpp
@@ -28,45 +28,50 @@ int main()
     int K = 50;                 // Número de veículos
     int timeOrder = 20;         // Tempo para resolver ORDER
     int timeVRP = 20;           // Tempo para resolver o VRP
-    int totalDays = 90;         // Dias percorridos
+    int totalDays = 20;         // Dias percorridos
+    vector<string> cities;
     time_t start, end;
     /*for(auto& p : filesystem::directory_iterator("resource\\Loggibud\\KMeans")){
         cout << p << endl; 
     }*/
-    string city("pa-");
-    string nameLocalInstance("cvrp-0-"+city);
-    for (int day = 62; day < totalDays; day++) {
-        string nameInstance(nameLocalInstance + to_string(day));
-        string filekmeans(
-            "resource\\Loggibud\\KMeans\\"+nameInstance+"-kmeans.json"
-        );
-        // string filename("resource\\Solomon\\c101-0.0.txt");
-        string filename("resource\\Loggibud\\cvrp-instances-1.0\\train\\pa-0\\"+nameInstance+".json");
-        string filevehicle("resource\\Solomon\\vei-homo.txt");
-        ifstream stream(filename);
-        string line;
-        string textJson("");
-        int ittt = 0;
-        cout << filename << endl;
-        while(!stream.fail()){
-            getline(stream,line);
-            textJson += line; 
+    // cities.push_back("pa-");
+    cities.push_back("df-");
+    cities.push_back("rj-");
+    for(string city : cities){
+        string nameLocalInstance("cvrp-0-"+city);
+        for (int day = 0; day < totalDays; day++) {
+            string nameInstance(nameLocalInstance + to_string(day));
+            string filekmeans(
+                "resource\\Loggibud\\KMeans\\"+nameInstance+"-kmeans.json"
+            );
+            // string filename("resource\\Solomon\\c101-0.0.txt");
+            string filename("resource\\Loggibud\\cvrp-instances-1.0\\train\\pa-0\\"+nameInstance+".json");
+            string filevehicle("resource\\Solomon\\vei-homo.txt");
+            ifstream stream(filename);
+            string line;
+            string textJson("");
+            int ittt = 0;
+            cout << filename << endl;
+            while(!stream.fail()){
+                getline(stream,line);
+                textJson += line; 
+            }
+            Json::Value root;
+            Json::Reader readerJson;
+            readerJson.parse(textJson, root);
+            const Json::Value deliveries = root["deliveries"];
+            LoggibudInstance reader = LoggibudInstance();
+            N = deliveries.size()+1;
+            CVRP problem = reader.readInput(filename, filevehicle, N, K);
+            double time_execution = problem.solveKmeansSeriable(
+                filekmeans, 
+                timeOrder,
+                timeVRP,
+                ALPHA,
+                nameInstance
+            );
+            problem.outputJson(problem.clusters_solved, nameInstance, time_execution);
         }
-        Json::Value root;
-        Json::Reader readerJson;
-        readerJson.parse(textJson, root);
-        const Json::Value deliveries = root["deliveries"];
-        LoggibudInstance reader = LoggibudInstance();
-        N = deliveries.size()+1;
-        CVRP problem = reader.readInput(filename, filevehicle, N, K);
-        double time_execution = problem.solveKmeansSeriable(
-            filekmeans, 
-            timeOrder,
-            timeVRP,
-            ALPHA,
-            nameInstance
-        );
-        problem.outputJson(problem.clusters_solved, nameInstance, time_execution);
     }
     // Solution solution = problem.solveWithKmeans(
     //     timeOrder,timeVRP, N, K, ALPHA, root["name"].asString()
--- config/generateCsv.py
+++ config/generateCsv.py
@@ -1,24 +1,38 @@
+import os
 import csv
+import json
+from loggibud.v1.eval.task1 import evaluate_solution
 # Gerar 1 arquivo Geral sobre dados comparativos
 # Algorithm | input | K | distance | n_veiculos | VAR(dif de max e min de pacotes/veiculos) | time 
 # Ler arquivo por arquivo
 # Captar os dados desejados
-def generateGeneralCsv(path_outcsv: str, city: str):
+def generateGeneralCsv(path_outcsv: str, city: str, output: str, path_input: str):
     head = ["Algorithm", "input", "K", "distance", "n_veiculos", "time"]
     f = open(path_outcsv, 'w', newline='', encoding='utf-8')
     w = csv.writer(f)
     # Construir Cabeçalho
     w.writerow(head) 
     # Read city per city
-    for instance in output_data:
-        root = instance.root
-        line = ["K-PPRRF"]
+    outputs = [
+        os.path.join(output, solution) 
+        for solution in os.listdir(output)
+    ]
+    for solution in outputs:
+        path_broke = solution.split('/')
+        name_instance = path_broke[len(path_broke)-1]
+        instance = path_input + name_instance
+        jfile = open(solution)
+        root = json.load(jfile) 
+        line = ["K-PPR-RF"]
         line.append(root['name'])
         line.append(root['k-regions'])
-        line.append(extractDistance(root))
+        line.append(evaluate_solution(instance, solution))
         line.append(root['total_vehicles'])
         line.append(root['time'])
         w.writerow(line)
+        instance_broke = instance.split('.')
+        name_broke = instance_broke.split('-')
+        day = name_broke[len(name_broke)-1]
         generateEspecificDay(city, day, root)
 
     w.close()
@@ -26,17 +40,23 @@ def generateGeneralCsv(path_outcsv: str, city: str):
 # Gerar 1 arquivo especifico sobre os veículos name-day
 # id_vehicle | capacity_used | n_deliveries
 def generateEspecificDay(city: str, day: int, root):
-    path_out = "caminho/a/definir/"+city+"/especificDay-"+day+".csv"
+    path_out = "output/csvs/"+city+"/especificDay-"+day+".csv"
     head = ["id_vehicle", "capacity_used", "n_deliveries"]
     f = open(path_out, 'w', newline='', encoding = 'utf-8')
-    w = csv.writerow(head)
+    w = csv.writer(f)
+    w.writerow(head)
     for id in range(len(root['vehicles'])):
         line = [id]
         line.append(computeCapacityRoute(root['vehicles'][id]))
-        line.append(len(root['vehicles']['deliveries']))
+        line.append(len(root['vehicles'][id]['deliveries']))
         w.writerow(line)
     w.close()
 
-def computeCapacityRoute(deliveries):
+def computeCapacityRoute(vehicle):
+    s = 0
+    for delivery in vehicle['deliveries']:
+        s += delivery['size']
+    return s
 
-def extractDistance(root):
+def main():
+    
